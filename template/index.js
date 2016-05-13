f7ab47ea60220d298fdc97a508476e08a3070d3f
qingwei.li <cinwell.li@gmail.com>
2016-05-13 11:28:26 +0800

    update comment

--- template/index.js
+++ template/index.js
@@ -1,5 +1,5 @@
 /**
- * @param  {object} cooking - add, remove and config
+ * @param  {object} cooking - add, remove, _userConfig and config
  * @param  {*} options - custom option
  */
 module.exports = function (cooking, options) {
